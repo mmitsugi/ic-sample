5adc4d23a18e9f7e5c3b36208aeeda31d082b65a
Masanori Mitsugi <mitsugi@linux.vnet.ibm.com>
2021-12-27 15:26:35 +0900

    Add port setting to flask

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ def download_log():
     return send_file(LOG_FILE, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
